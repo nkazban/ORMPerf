3d710aec941714f71b0a52f7634d5a5eb9af1256
Nazar Kazban <kazbannazar@gmail.com>
2020-11-26 17:13:27 +0200

    Correct namespaces

--- ORMPerf.Core/ILogger.cs
+++ ORMPerf.Core/ILogger.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
-
-namespace ORMPerf.Core
+﻿namespace ORMPerf.Core
 {
     public interface ILogger
     {
--- ORMPerf.Core/SimpleModel.cs
+++ ORMPerf.Core/SimpleModel.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace ORMPerf
+namespace ORMPerf.Core
 {
     public class SimpleModel
     {
